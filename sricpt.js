933ff7c3a8c8cc1f310755cb95ee3badfa49a605
Mhlengi <57716938+Mhlengi12@users.noreply.github.com>
2020-09-21 16:03:54 +0200

    Add files via upload
    Technical CV now has an animated scroll bar along with certificate containers. 
    Good navigation
    Good Viewpoint
    Currently no bugs

--- sricpt.js
+++ sricpt.js
@@ -1,6 +1,21 @@
+window.onload = function()
+{
+    let progress = document.getElementById('scrollBar');
+      let totalHeight = document.body.scrollHeight - window.innerHeight;
+      window.onscroll = function()
+      {
+      let progressHeight = (window.pageYOffset / totalHeight) * 100;
+      progress.style.height = progressHeight + "%";
+      }
+
+}
+
+
+/*
 const progressBar = document.getElementsByClassName('progress-bar')[0]
 setInterval(() => {
     const computedStyle = getComputedStyle(progressBar)
     const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
     progressBar.style.setProperty('--width',width + .01)
 }, 20)
+*/
