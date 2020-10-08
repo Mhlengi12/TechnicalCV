7cc730f4f9dc21d73526d363805280c729c92ec5
Mhlengi <57716938+Mhlengi12@users.noreply.github.com>
2020-10-08 21:06:18 +0200

    Add files via upload
    Embed CV document(document not commited here)
    Added contact method (email method)
    Added backgrounds for about text
    Added latest Cirtificate

--- app.js
+++ app.js
@@ -111,3 +111,4 @@ function requestUserRepos(username) {
     xhr.send();
 
 }
+
--- sricpt.js
+++ sricpt.js
@@ -9,13 +9,35 @@ window.onload = function()
       progress.style.height = progressHeight + "%";
       }
 
-      if(progressHeight >= 70)
+}
+
+window.addEventListener('load',(event) => {
+  document.querySelector('.container .skills .percent .progress').classList.add('animate8');
+});
+
+/*
+let timer = 0;
+window.addEventListener('load',(event)=>{
+  let intersectionObserver = new IntersectionObserver(entries =>{
+    entries.forEach(entry => {
+      if(entry.isIntersecting)
       {
-        //document.getElementById('loadbar').style.animation ='animate2 4s ease-in-out forwards';
+        setTimeout(function() {entry.target.classList.add('animate');}, timer);
+        timer += 50;
+        intersectionObserver.unobserve(entry.target);
+        setTimeout(function() {timer = 0}, 1000);
       }
+      
+    });
+  });
 
+  document.querySelectorAll('.container .skills .percent .progress').forEach(obj =>{
+    intersectionObserver.observe(obj);
+  });
+
+});
   
-
+*/
 /*
     var qouteid;
     while(qouteid != "27")
@@ -38,17 +60,32 @@ window.onload = function()
     });
 
     }
+    OR
+    $.getJSON(
+    "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote", 
+    function(data4){
+        document.getElementById("qoute").innerHTML = data4.starWarsQuote;
+});
 */
-}
 
-var large = document.getElementsByClassName("cert")
-large.onclick = function enlarge()
-{
-    $("div").click(function()
-{
-    $("div").animate({width:'250px'} ,{height:'200px'}, 1000);
 
-})
+function sendEmail()
+    {
+      document.getElementById("btn1").style.animation = "myframes 1s linear forwards"; 
 
-}
+      var subjectval = document.getElementById("Subject").nodeValue;
+      var bodyval = document.getElementById("Body").nodeValue;
+      
 
+      Email.send({
+      Host : "smtp.gmail.com",
+      Username : "Mshadow0012@gmail.com",
+      Password : "HinokamiKagura12",
+      To : 'Mhlengi12@gmail.com',
+      From : "Mshadow0012@gmail.com",
+      Subject : subjectval,
+      Body : bodyval
+  }).then(
+    message => alert(message)
+  );
+    }
